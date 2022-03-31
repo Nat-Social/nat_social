18c15f663a6338177ab13b502bde85948b2c5064
Franklin Lucena <franklinl@epidataconsulting.cm>
2022-03-30 23:53:36 -0400

    Initial commit

---
+++ app/config/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAnEMxVTfxsVahaeuuDS3Y8EFXEPC2rcZo",
+  authDomain: "natsocialapp.firebaseapp.com",
+  projectId: "natsocialapp",
+  storageBucket: "natsocialapp.appspot.com",
+  messagingSenderId: "764384623566",
+  appId: "1:764384623566:web:48466251c691d10347899f"
+};
+export default firebaseConfig;
